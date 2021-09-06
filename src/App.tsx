9f44826ab9d3e96ac7bacf431890d274a9a4291c
lijinxing <galaxyx99@163.com>
2021-09-07 00:16:25 +0800

    feat(*): accomplish basic list **add**

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, Fragment} from 'react';
 import './App.css';
 
+interface Todo {
+  id: number;
+  content: string;
+  isDone: boolean;
+}
+
+function List(props : {todos : Todo[]}) : JSX.Element {
+  return (
+    <>
+      {props.todos.map((item : Todo) => (
+          <Fragment key={item.id}>
+            <div className="item" >{item.isDone ? ' done' : 'doing'} : {item.content}</div>
+          </Fragment>
+      ))}
+    </>
+  )
+}
+
 function App() {
+
+  const [inputText, setInputText] = useState('')
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  /**
+   * 监听输入事件
+   * @param e inputEvent
+   */
+  function handleInput(e:any) {
+    const text = e.target.value
+    setInputText(text)
+  }
+
+  /**
+   * 监听回车键
+   * @param e inputEvent
+   */
+  function checkInput(e:any) {
+    if(e.keyCode === 13) {
+      setTodos([...todos, {id: (new Date()).getTime(), content: inputText, isDone: false}])
+      setInputText('')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">Todo</div>
+      <div className="todos">
+        <div className="input">
+          <input type="text" value={inputText} onChange={handleInput} onKeyUp={(e)=> checkInput(e)}/>
+        </div>
+        <div className="list">
+          <List todos={todos}></List>
+        </div>
+      </div>
     </div>
   );
 }
