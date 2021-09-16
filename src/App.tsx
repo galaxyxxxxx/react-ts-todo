5bda09822fc5c9ff92ff419a5e69529ad70b1b59
lijinxing <galaxyx99@163.com>
2021-09-16 10:29:21 +0800

    feat(*): 更改UI结构，增加footer的UI层

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from 'react';
+import React, { useState, Fragment } from 'react';
 import './App.css';
 
 interface Todo {
@@ -7,54 +7,93 @@ interface Todo {
   isDone: boolean;
 }
 
-function List(props : {todos : Todo[]}) : JSX.Element {
+/**
+ * TODOS列表
+ * @param props 
+ * @returns 
+ */
+function List(props: { todos: Todo[] }): JSX.Element {
   return (
     <>
-      {props.todos.map((item : Todo) => (
-          <Fragment key={item.id}>
-            <div className="item" >{item.isDone ? ' done' : 'doing'} : {item.content}</div>
-          </Fragment>
+      {props.todos.map((item: Todo) => (
+        <Fragment key={item.id}>
+          <div className="item" >{item.isDone ? ' done' : 'doing'} : {item.content}</div>
+        </Fragment>
       ))}
     </>
   )
 }
 
+/**
+ * 底部切换栏
+ * @returns 
+ */
+function Tabs(props: {curShowing : number}) {
+  return (
+    <footer>
+      <div className="todo-count">* items left</div>
+      <div className="filters">
+        <li><a href="#/">ALL</a></li>
+        <li><a href="#/active">Active</a></li>
+        <li><a href="#/completed">Completed</a></li>
+      </div>
+    </footer>
+  )
+}
+
 function App() {
 
-  const [inputText, setInputText] = useState('')
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [inputText, setInputText] = useState('') //输入文本
+  const [todos, setTodos] = useState<Todo[]>([]) //todos列表
+  const [curShowing, setCurShowing] = useState<number>(0) //当前展示区类别
 
   /**
    * 监听输入事件
    * @param e inputEvent
    */
-  function handleInput(e:any) {
+  function handleInput(e: any) {
     const text = e.target.value
     setInputText(text)
+  }
+
+  /**
+   * 核对输入内容是否合法
+   * @param {string} txt 输入框文本
+   * @returns {Boolean}
+   */
+  function checkInputText(txt: string) {
+    if (txt === '') {
+      alert('Please enter something')
+      return false
+    } else {
+      return true
+    }
   }
 
   /**
    * 监听回车键
    * @param e inputEvent
    */
-  function checkInput(e:any) {
-    if(e.keyCode === 13) {
-      setTodos([...todos, {id: (new Date()).getTime(), content: inputText, isDone: false}])
-      setInputText('')
+  function checkInput(e: any) {
+    if (e.keyCode === 13) {
+      if (checkInputText(inputText)) {
+        setTodos([...todos, { id: (new Date()).getTime(), content: inputText, isDone: false }])
+        setInputText('')
+      }
     }
   }
 
   return (
     <div className="App">
-      <div className="title">Todo</div>
-      <div className="todos">
-        <div className="input">
-          <input type="text" value={inputText} onChange={handleInput} onKeyUp={(e)=> checkInput(e)}/>
-        </div>
-        <div className="list">
-          <List todos={todos}></List>
-        </div>
-      </div>
+      <header>
+        <h1>Todo</h1>
+        <input type="text" value={inputText} onChange={handleInput} onKeyUp={(e) => checkInput(e)} />
+      </header>
+      <section className="main">
+        <List todos={todos}></List>
+        <Tabs curShowing={curShowing}></Tabs>
+      </section>
+        
     </div>
   );
 }
